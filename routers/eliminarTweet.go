517348b7447fb742e00800fbb419981442f23e3f
SergioGRivera1812 <sergiodidierg@gmail.com>
2022-02-03 21:36:05 -0600

    BorroTweet

---
+++ routers/eliminarTweet.go
@@ -0,0 +1,23 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/SergioGRivera1812/App-Curso/bd"
+)
+
+func EliminarTweet(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parametro ID ", http.StatusBadRequest)
+		return
+	}
+
+	err := bd.BorroTweet(ID, IDUsuario)
+	if err != nil {
+		http.Error(w, "Ocurrio un error al intentar borrar el tweet"+err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
